b3fc8e85a1b127b5737b8a7cb0737bc7ce733e1a
Vanessa Su <vnessa.su@gmail.com>
2021-03-26 12:53:03 -0400

    Hide current card when answer button is clicked, reveal next card and console log the button value.

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,10 @@ $(document).ready(function(){
     $(this).hide();
     $("#questionOne").show();
   });
+  $(".answer-button").click(function(){
+    let answerType = $(this).val();
+    console.log(answerType + " answer button pressed for");
+    $(this).closest(".card").hide();
+    $(this).closest(".card").next(".question-card").show();
+  });
 });
