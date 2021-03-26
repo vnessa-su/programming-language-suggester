8f747238630aae85d2c2199238728a8d592b5929
Vanessa Su <vnessa.su@gmail.com>
2021-03-26 13:24:19 -0400

    Add ability to keep track of how many times an answer type was selected.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,30 @@
 $(document).ready(function(){
+  let csharpCount = 0;
+  let rubyCount = 0;
+  let javascriptCount = 0;
   $("#startQuizButton").click(function(){
     $(this).hide();
     $("#firstQuestion").show();
   });
   $(".answer-button").click(function(){
-    let answerType = $(this).val();
-    console.log(answerType + " answer button pressed for");
-    let cardId = $(this).closest(".card").attr("id");
-    console.log(cardId);
-    console.log(cardId === "lastQuestion");
+    const answerType = $(this).val();
+    console.log(answerType + " answer button pressed");
+    const cardId = $(this).closest(".card").attr("id");
+
+    if(answerType === "csharp-answer") {
+      csharpCount++;
+    } else if(answerType === "ruby-answer") {
+      rubyCount++;
+    } else if(answerType === "javascript-answer") {
+      javascriptCount++;
+    } else {
+      throw new Error("Invalid answer-button value");
+    }
+
+    console.log('csharp count: ' + csharpCount);
+    console.log("ruby count: " + rubyCount);
+    console.log("javascript count: " + javascriptCount);
+
     $(this).closest(".card").hide();
     if(cardId === "lastQuestion"){
       $("#suggestedLanguageDisplay").show();
